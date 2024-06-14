e6a659db0264c5cb035142cc295a646e10f69a14
DarkDev <darkdev_brand@riseup.net>
2024-06-14 03:44:31 +0300

    intial quiz route

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	// import {  } from '$widgets';
+	// import { } from '$entities';
+	// import {  } from '$shared';
+</script>
+
+<svelte:head>
+	<title>QIUZ - Archaeo stories</title>
+</svelte:head>
